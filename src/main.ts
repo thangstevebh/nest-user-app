32e65f752637931c6e32e056d8a27514c7a4298b
thangstevebui <bui.thang@vn.lion-garden.com>
2025-07-15 15:47:40 +0700

    ✨ Test
    test

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const PORT = process.env.PORT ?? 3000;
   const app = await NestFactory.create(AppModule);
+  console.log(`Environment: ${process.env.NODE_ENV}`);
   await app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
   });
